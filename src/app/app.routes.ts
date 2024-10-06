89d67dd5778ce5b190dc3c6c08f8f1e0b3e6b027
philister16 <phsn16@gmail.com>
2024-10-06 11:52:04 +0200

    Routing, components and services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // Redirect to home as default
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+
+  // Unguarded routes
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./auth/auth/auth.component').then(m => m.AuthComponent)
+      },
+      {
+        path: 'action',
+        loadComponent: () => import('./auth/action/action.component').then(m => m.ActionComponent)
+      }
+    ]
+  },
+
+  // Guarded routes
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'account',
+        loadComponent: () => import('./account/account.component').then(m => m.AccountComponent)
+      }
+    ]
+  },
+
+  // 404
+  {
+    path: '**',
+    loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent)
+  }
+];
