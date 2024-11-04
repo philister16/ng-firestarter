5b84eb02ef90328e8f29b0af1315b5396751a311
philister16 <phsn16@gmail.com>
2024-11-04 21:17:46 +0100

    Refactor unauthorized component

--- src/app/auth/unauthorized/unauthorized.component.ts
+++ src/app/auth/unauthorized/unauthorized.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { UnauthorizedReason } from '../auth.service';
@@ -9,12 +9,13 @@ import { UnauthorizedReason } from '../auth.service';
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './unauthorized.component.html',
-  styleUrl: './unauthorized.component.css'
+  styleUrls: ['./unauthorized.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UnauthorizedComponent implements OnInit {
   private route = inject(ActivatedRoute);
 
-  message = signal<string>('You are not authorized to access this page.');
+  content: { heading: string, message: string } = { heading: 'Unauthorized Access', message: 'You are not authorized to access this page.' };
 
   ngOnInit() {
     const reason = this.route.snapshot.queryParamMap.get('reason');
@@ -24,13 +25,13 @@ export class UnauthorizedComponent implements OnInit {
   private updateMessage(reason: string | null) {
     switch (reason) {
       case UnauthorizedReason.EMAIL_NOT_VERIFIED:
-        this.message.set('Please verify your email address to access this page.');
+        this.content = { heading: 'Email Not Verified', message: 'Please verify your email address to access this page.' };
         break;
       case UnauthorizedReason.INSUFFICIENT_PERMISSIONS:
-        this.message.set('You do not have the necessary permissions to access this page.');
+        this.content = { heading: 'Insufficient Permissions', message: 'You do not have the necessary permissions to access this page.' };
         break;
       default:
-        this.message.set('You are not authorized to access this page.');
+        this.content = { heading: 'Unauthorized Access', message: 'You are not authorized to access this page.' };
     }
   }
 }
